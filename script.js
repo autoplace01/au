27f7bd7d195f14b61e1bf335e7bfb11e35d59c66
autoplace01 <ajahadahab19@gmail.com>
2025-10-03 09:25:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,3 @@ function ouvrirDetails(nom, prix) {
   alert(`Détails de la voiture:\nNom: nom:{prix} MAD`);
 }
 const params = new URLSearchParams(window.location.search);
-doument.getElementById("nomVoiture").textContent = `params.get("marque"){params.get("modele")}`;
-document.getElementById("prixVoiture").textContent = `${params.get("prix")} MAD`;
-doument.getElementById("nomVoiture").textContent = `params.get("marque"){params.get("modele")}`;
-document.getElementById("prixVoiture").textContent = `${params.get("prix")} MAD`;
-document.getElementById('marque').textContent = data.marque;
-document.getElementById('annee').textContent = data.annee;
-document.getElementById('kilometrage').textContent = data.kilometrage;
-document.getElementById('ville').textContent = data.ville;
-document.getElementById('etat').textContent = data.etat;
-document.getElementById('carburant').textContent = data.carburant;
-document.getElementById('description').textContent = data.description;
-document.getElementById('image').src = data.image;
-
