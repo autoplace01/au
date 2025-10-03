024e1dc4d66f6f1c0ec246bbdc492838411f1ea6
autoplace01 <ajahadahab19@gmail.com>
2025-10-03 09:31:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ function filtrerVoitures() {
 const voitures = document.querySelectorAll('.voiture');
 voitures.forEach(voiture => {
     const voitureMarque = voiture.dataset.marque;
-    const voiturePrix = parseInt(voiture.dataset.prix);
+  document.getElementById('marque').textContent = data.marque;
+document.getElementById('annee').textContent = data.annee;
+  document.getElementById('kilometrage').textContent = data.kilometrage;
+document.getElementById('ville').textContent = data.ville;
+document.getElementById('etat').textContent = data.etat;
+document.getElementById('carburant').textContent = data.carburant;
+document.getElementById('description').textContent = data.description;
+document.getElementById('image').src = data.image;
+const voiturePrix = parseInt(voiture.dataset.prix);
 const marqueMatch = (marque === 'all' || voitureMarque === marque);
     const prixMatch = (prixMax === 0 || voiturePrix <= prixMax);
 voiture.style.display = (marqueMatch && prixMatch) ? 'block' : 'none';
@@ -15,3 +23,4 @@ function ouvrirDetails(nom, prix) {
   alert(`Détails de la voiture:\nNom: nom:{prix} MAD`);
 }
 const params = new URLSearchParams(window.location.search);
+
