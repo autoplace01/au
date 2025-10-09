275eba2a77d1859ec94bddd0fecfdfd9b010a13e
autoplace01 <ajahadahab19@gmail.com>
2025-10-09 15:01:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ function filtrerVoitures() {
 function ouvrirDetails(nom, prix) {
   alert(`Détails de la voiture :\nNom : nom :{prix} MAD`);
 }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBWkbHz-fi15InLnJZg7i75VRDpBLMY3PM",
+  authDomain: "autoplace01-1f653.firebaseapp.com",
+  projectId: "autoplace01-1f653",
+  storageBucket: "autoplace01-1f653.firebasestorage.app",
+  messagingSenderId: "361871095391",
+  appId: "1:361871095391:web:f5d7b703c8c0f1e6188dea",
+  measurementId: "G-L9MK4SJ7J5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
